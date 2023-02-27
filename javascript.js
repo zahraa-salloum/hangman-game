a32d8f6e249a4a88a296f47c8b28b951f5551aee
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-27 23:26:54 +0200

    Randomize category display

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 
 const letters = document.getElementById("letters");
+let hint = document.getElementById("hint");
 for (let i = 65; i < 91; i++) {
     let button = document.createElement("button");
     button.innerText = String.fromCharCode(i);
@@ -7,8 +8,18 @@ for (let i = 65; i < 91; i++) {
     button.classList.add("button");
   }
 
-  let dict = {
-    fruits: ["Strawberry","Orange","Apple","Blueberry","Mandarin","Pineapple","Pomegranate","Watermelon"],
-    animals: ["Elephant","Cow","Fox","Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra","Ant","Ladybug"],
+let dict = {
+    fruits: ["Strawberry","Orange","Apple","Blueberry","Mandarin","Pineapple","Pomegranate","Watermelon","kiwi"],
+    animals: ["Elephant","Cow","Fox","Squirrel", "Panther", "Walrus", "Zebra","Ant","Ladybug"],
     countries: ["Lebanon","Syria","Russia","India","Hungary","Kyrgyzstan","Switzerland","Zimbabwe","Dominica"],
   };
+
+let choose_category = Math.floor(Math.random()*3)
+if(choose_category == 0)
+    hint.innerHTML = "Hint: It is a fruit"
+else if (choose_category == 1)
+     hint.innerHTML = "Hint: It is an animal"
+else
+    hint.innerHTML = "Hint: It is a country"
+  console.log(choose_category)
+  

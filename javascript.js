3711feeea304a59fc2a44a3bf3cb58660359f9a3
zahraa-salloum <zahraa.h.salloum@gmail.com>
2023-02-27 23:15:02 +0200

    Added dictionary

--- javascript.js
+++ javascript.js
@@ -6,3 +6,9 @@ for (let i = 65; i < 91; i++) {
     letters.append(button);
     button.classList.add("button");
   }
+
+  let dict = {
+    fruits: ["Strawberry","Orange","Apple","Blueberry","Mandarin","Pineapple","Pomegranate","Watermelon"],
+    animals: ["Elephant","Cow","Fox","Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra","Ant","Ladybug"],
+    countries: ["Lebanon","Syria","Russia","India","Hungary","Kyrgyzstan","Switzerland","Zimbabwe","Dominica"],
+  };
